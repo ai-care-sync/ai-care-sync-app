45d3a4d2fd5696512fae5899c9bb12bc01f7e0b3
nav <nksriniv@ucsc.edu>
2023-12-28 13:35:08 -0800

    implemented splash screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,35 @@ import React from 'react';
 import Modal from './components/modal'
 import MainComponent from './components/mainComponent';
 import MainHeader from './components/MainHeader';
+import SplashScreen from './components/splashScreen';
 // import Modal from './Modal'; // Import your Modal component or any other component
 
 // Main App component
 function App() {
+    
     // State to control the visibility of the Modal (if you're using it)
     const [isModalOpen, setModalOpen] = React.useState(false);
+    //state to control the visibility of the splash screen
+    const [showMainComponent, setshowMainComponent] = React.useState(false);
 
     // Function to toggle the modal's visibility
     const toggleModal = () => {
         setModalOpen(!isModalOpen);
     };
 
+    const navigateToMainComponent = () => {
+        setshowMainComponent(!showMainComponent);
+    }
+
     return (
         <div className="App">
             {/* <header className="App-header"> */}
             {/* <MainHeader/> */}
-            <MainComponent/>
+            {!showMainComponent ? (
+                <SplashScreen navigateToMainComponent={navigateToMainComponent} />
+            ) : (
+                <MainComponent />
+            )}
             {/* </header> */}
         </div>
     );
